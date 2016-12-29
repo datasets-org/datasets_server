ec0bd6e57451a2d8593c3cab160a45954a8d91ca
Vit Listik <tivvit@seznam.cz>
2016-12-29 14:22:08 +0100

    sever in compose

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ d.scan(["data"])
 
 s.load()
 print("*" * 50)
+
 # print(s.data)
-
 # s.delete("8b88a424-dbd8-4032-8be7-a930a415b9a5")
-
-# d.use("8b88a424-dbd8-4032-8be7-a930a415b9a5", {"user": "tivvit", "op": "a"})
+# d.use("8b88a424-dbd8-4032-8be7-a930a415b9a5", {"user": "aaa", "op": "a"})
 
